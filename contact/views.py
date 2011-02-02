0ff143cb415a2164571819737fcc741db6588248
Oksana Slusarenko <oksana.slu@gmail.com>
2011-02-02 22:42:43 +0200

    style code according PEP8

--- contact/views.py
+++ contact/views.py
@@ -4,6 +4,6 @@ from testcups.contact.models import Contact
 
 
 def contact(request):
-    contact = Contact.objects.get(id=1)    
+    contact = Contact.objects.get(id=1)
     return render_to_response('contact.html',
-                              context_instance=RequestContext(request, {'contact': contact}))
+           context_instance=RequestContext(request, {'contact': contact}))
