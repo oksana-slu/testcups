843cc4183df462bda590f8c16079495bc5fbe3cd
Oksana Slusarenko <oksana.slu@gmail.com>
2011-02-03 06:04:21 +0200

    form in template

--- contact/models.py
+++ contact/models.py
@@ -1,15 +1,22 @@
 from django.db import models
+from django.forms import ModelForm
 
 
 class Contact(models.Model):
     name = models.CharField(max_length=100)
     last_name = models.CharField(blank=True, max_length=100)
-    birth = models.DateField(blank=True)
+    birth = models.DateField(blank=True, null=True)
     bio = models.TextField(blank=True)
     email = models.EmailField(max_length=75)
     jabber = models.CharField(blank=True, max_length=100)
     skype = models.CharField(blank=True, max_length=100)
-    other_contacts = models.CharField(blank=True, max_length=200)
+    other_contacts = models.TextField(blank=True)
 
     def __unicode__(self):
         return self.name
+        
+        
+class ContactForm(ModelForm):
+    class Meta:
+        model = Contact
+
--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from testcups.contact.models import Contact
+from testcups.contact.models import Contact, ContactForm
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect, HttpResponse
 
 
 def contact(request):
@@ -10,5 +12,44 @@ def contact(request):
            
            
 def edit_contact(request):
-    return render_to_response('edit_contact.html',
-           context_instance=RequestContext(request, {}))
+    initial = None
+    if request.user.is_authenticated():
+        user = request.user
+        item_contact = Contact.objects.get(name=user)
+        initial = dict(name=item_contact.name,
+                       last_name=item_contact.last_name,
+                       birth=item_contact.birth,
+                       bio=item_contact.bio,
+                       email=item_contact.email,
+                       jabber=item_contact.jabber,
+                       skype=item_contact.skype,
+                       other_contacts=item_contact.other_contacts)                       
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            if request.user.is_authenticated():
+                item_contact.name=cleaned_data['name']
+                item_contact.last_name=cleaned_data['last_name']
+                item_contact.birth=cleaned_data['birth']
+                item_contact.bio=cleaned_data['bio']
+                item_contact.email=cleaned_data['email']
+                item_contact.jabber=cleaned_data['jabber']
+                item_contact.skype=cleaned_data['skype']
+                item_contact.other_contacts=cleaned_data['other_contacts']
+                item_contact.save()
+            else:
+                Contact_object = Contact(name=cleaned_data['name'],
+                                     last_name=cleaned_data['last_name'],
+                                     ibirth=cleaned_data['birth'],
+                                     bio=cleaned_data['bio'],
+                                     email=cleaned_data['email'],
+                                     jabber=cleaned_data['jabber'],
+                                     skype=cleaned_data['skype'],
+                                     other_contacts=cleaned_data['other_contacts'])            
+                Contact_object.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = ContactForm(initial=initial)
+    return render_to_response('edit_contact.html', {},
+           context_instance=RequestContext(request, {'form': form}))    
