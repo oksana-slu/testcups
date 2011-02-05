828cd19b90208adb1418b9e50de37a111d0c21b3
Oksana Slusarenko <oksana.slu@gmail.com>
2011-02-05 05:16:32 +0200

    test pep8

--- urls.py
+++ urls.py
@@ -5,9 +5,9 @@ admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    url(r'', include("testcups.contact.urls")),    
+    url(r'', include("testcups.contact.urls")),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login',
         {'template_name': 'login.html'}, name='auth_login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', name='auth_logout'),    
+    url(r'^logout/$', 'django.contrib.auth.views.logout', name='auth_logout'),
     (r'^admin/', include(admin.site.urls)),
 )
